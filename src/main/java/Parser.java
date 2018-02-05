18b2d0bb273e114c147bba0ac73d63b3f924c449
Richard Luong <r-luong@users.noreply.github.com>
2018-02-05 15:33:07 +1100

    Implemented isValidExpr(), Passes all invalidExpression tests

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -20,14 +20,22 @@ public class Parser {
     }
 
 	/**
-	 * Gets a string representing a propositional logic expression and analyses it to creates a list of tokens that
-     * correlate to the symbols in the expression such as the variables and connectives. In the end return this list
+	 * Gets a string representing a propositional logic expression and iterates over the characters to create a list of
+     * tokens that represent the symbols in the expression such as the variables and connectives. In the end return
+     * this list
 	 * @param inputStr prop logic expression from the user as a string
 	 * @return a list of tokens
 	 */
 	public ArrayList<Token> createTokensFromInput(String inputStr) {
-	    // TODO: Implement
-		return new ArrayList<Token>();
+        ArrayList<Token> tokens = new ArrayList<>();
+        char[] charArr = inputStr.toCharArray();
+        for (char c: charArr) {
+            // Ignore whitespace
+            if (c == ' ') continue;
+            Token currToken = Token.charToToken(c);
+            tokens.add(currToken);
+        }
+        return tokens;
 	}
 
     /**
@@ -36,8 +44,117 @@ public class Parser {
      * @return true if the tokens form a valid expression, false otherwise
      */
     public boolean isValidExpr(ArrayList<Token> tokens) {
-        // TODO: Implement
-        return false;
+        // An empty expression is invalid
+        if (tokens.isEmpty()) return false;
+        // Check there is no unclosed pair of parentheses
+        if (!isParenBalanced(tokens)) return false;
+        // Checks that there are no two variables, connective or pair of matching parentheses are adjacent to each other
+        if (!adjacencyTest(tokens)) return false;
+        // Check all operators have a valid sub expr on their left or right
+        if (!operatorTest(tokens)) return false;
+        return true;
+
+    }
+
+    // Helper methods to determine if an expression is valid
+
+    /**
+     * Checks if the list of tokens has no unmatched pairs of parenthesis
+     * @param tokens Tokens representing the expression
+     * @return true if there are no unmatched pairs of parenthesis
+     */
+    private boolean isParenBalanced(ArrayList<Token> tokens) {
+        Stack<Token> parenStack = new Stack<>();
+        for (Token currToken: tokens) {
+            if (currToken == Token.L_PAREN) {
+                parenStack.add(currToken);
+            } else if (currToken == Token.R_PAREN) {
+                if (parenStack.isEmpty()) {
+                    return false;
+                } else {
+                    parenStack.pop();
+                }
+            }
+        }
+        // If the stack is not empty then there is at least one unmatched parenthesis pair somewhere
+        return parenStack.isEmpty();
+    }
+
+    /**
+     * Checks that there are no two variables, connective, pair of matching parentheses are adjacent to each other
+     * @param tokens List of tokens representing the prop logic expression
+     * @return true if it passes the test
+     */
+    private boolean adjacencyTest(ArrayList<Token> tokens) {
+        for (int i = 0; i < tokens.size()-1; ++i) {
+            Token currToken = tokens.get(i);
+            Token nextToken = tokens.get(i+1);
+            if (Token.isVar(currToken) && Token.isVar(nextToken)) {
+                return false;
+            }  else if (Token.isConnective(currToken) && Token.isConnective(nextToken)) {
+                return false;
+            } else if (currToken == Token.L_PAREN && nextToken == Token.R_PAREN) {
+                return false;
+            } else if (currToken == Token.NOT && Token.isConnective(nextToken)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    /**
+     * Checks all operators (special case with NOT) have a valid sub expr to their left and right through the
+     * existence of a variable
+     * @param tokens List of tokens representing the prop logic expression
+     * @return true or false
+     */
+    private boolean operatorTest(ArrayList<Token> tokens) {
+        // First find all the connectives in the expr and note their index
+        ArrayList<Integer> operatorIndexes = new ArrayList<>();
+        for (int i = 0; i < tokens.size(); i++) {
+            Token currToken = tokens.get(i);
+            if (Token.isOperator(currToken)) {
+                operatorIndexes.add(i);
+            }
+        }
+        // Now check there are sub expressions on the left and right side by searching for the existence of a variable
+        for (int cIndex: operatorIndexes) {
+            boolean varFound = false;
+            Token currOperator = tokens.get(cIndex);
+            // Check LHS of the operator
+            // Special case for the NOT operator: Left of NOT if not the start, should be a connective or left paren
+            if (currOperator == Token.NOT && cIndex != 0) {
+                Token leftToken = tokens.get(cIndex-1);
+                if (Token.isVar(leftToken) || leftToken == Token.R_PAREN)
+                    return false;
+            } else {
+                for (int i = cIndex-1; i >= 0; i--) {
+                    Token currToken = tokens.get(i);
+                    if (Token.isVar(currToken)) {
+                        varFound = true;
+                        break;
+                    } else if (Token.isOperator(currToken)) {
+                        // Early exit if the previous connective is found before a variable
+                        return false;
+                    }
+                }
+                if (!varFound) return false;
+            }
+            // Check RHS of the operator
+            varFound = false;
+            for (int i = cIndex+1; i < tokens.size(); i++) {
+                Token currToken = tokens.get(i);
+                if (Token.isVar(currToken)) {
+                    varFound = true;
+                    break;
+                } else if (Token.isOperator(currToken)) {
+                    // Early exit if the next connective is found before a variable
+                    return false;
+                }
+            }
+            if (!varFound) return false;
+        }
+        return true;
     }
 
     /**
@@ -58,7 +175,7 @@ public class Parser {
     private Set<Token> getVariables(ArrayList<Token> tokens) {
         // TODO: Implement
         // Order of variables is in the order they appear
-        return new LinkedHashSet<Token>();
+        return new LinkedHashSet<>();
     }
 	
 
